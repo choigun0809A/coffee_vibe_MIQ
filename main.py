1f79613fd0dddb4dfd82200a49206db605e01509
choigun0809 <choigun4@gmail.com>
2025-08-06 09:59:46 +0800

    1.1 update

--- main.py
+++ main.py
@@ -143,13 +143,16 @@ def google():
 def sitemap():
     xml = '''<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
   <url>
-    <loc>https://choistests.onrender.com</loc>
-    <lastmod>2025-08-01</lastmod>
-    <changefreq>weekly</changefreq>
+    <loc>https://choistests.onrender.com/</loc>
+    <lastmod>2025-08-05</lastmod>
     <priority>1.0</priority>
   </url>
-</urlset>'''
+
+
+</urlset>
+'''
     return Response(xml, mimetype='application/xml')
 
 
