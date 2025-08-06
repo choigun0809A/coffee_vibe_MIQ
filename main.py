5c03b0938d8c25f774926df54a5e37a779ea60a7
choigun0809 <choigun4@gmail.com>
2025-08-06 09:37:35 +0800

    0.8 update

--- main.py
+++ main.py
@@ -276,6 +276,17 @@ def admin():
 
 
 
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/favicon")
+def home():
+    return render_template("home.html")
+
+
 if __name__ == "__main__":
     app.run()
 
